9c62383b3c559232e4956e36f789960e95cbcab5
Rana Ockaili <rana.ockaili@foo-solutions.com>
2017-12-15 22:46:20 +0200

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    // FindOneAndUpdate
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     text: 'Eat lunch'
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        name: 'Mike'
+    }, {
+        $set: {
+            name: 'Rana'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    client.close();
+});
